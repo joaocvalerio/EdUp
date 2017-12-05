599c8a95a08683206c37e8c04fa3c601bd0777d3
joaovalerio <joaocvalerio@gmail.com>
2017-12-05 21:23:00 +0000

    small improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
 
   def full_name
-    self.first_name.capitalize + " " + self.last_name
+    "#{full_name.capitalize} #{last_name.capitalize}"
   end
 
   def publisher?
